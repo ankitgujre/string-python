976f9b7f9ceb10ee266d3e324117e21dc0e34c61
ankitgujre <ankitgujre374@gmail.com>
2026-02-04 11:30:47 +0530

    count length and string functions

---
+++ countLength.py
@@ -0,0 +1,9 @@
+st = input("Enter string: ")
+
+count = 0
+
+for ch in st:
+    count += 1
+
+print("Total characters =", count)
+print("Length of string =", len(st))
---
+++ string_functions.py
@@ -0,0 +1,10 @@
+str = "iI am a Coder"
+
+print(str.endswith("er"))
+
+print(str.capitalize())
+
+print(str.replace("Coder", "Developer"))
+
+print(str.count('a'))
+print(str.find('a'))
